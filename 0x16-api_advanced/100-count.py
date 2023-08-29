96cacdc5c7f410ca6d28c91608526b014a878c4c
Nice22 <benothouessou74@gmail.com>
2023-08-29 23:58:29 +0000

    count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,8 +2,9 @@
 """Contains the count_words function"""
 import requests
 
-
-def count_words(subreddit, word_list, found_list=[], after=None):
+def count_words(subreddit, word_list, found_dict=None, after=None):
+    if found_dict is None:
+        found_dict = {}
     '''Prints counts of given words found in hot posts of a given subreddit.
 
     Args:
@@ -12,32 +13,42 @@ def count_words(subreddit, word_list, found_list=[], after=None):
         found_list (obj): Key/value pairs of words/counts.
         after (str): The parameter for the next page of the API results.
     '''
+
     user_agent = {'User-agent': 'test45'}
-    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
-                         .format(subreddit, after), headers=user_agent)
+    posts = requests.get(f'http://www.reddit.com/r/{subreddit}/hot.json?after={after}', headers=user_agent)
+
     if after is None:
         word_list = [word.lower() for word in word_list]
 
     if posts.status_code == 200:
-        posts = posts.json()['data']
-        aft = posts['after']
-        posts = posts['children']
+        posts_data = posts.json()['data']
+        aft = posts_data['after']
+        posts = posts_data['children']
+
         for post in posts:
             title = post['data']['title'].lower()
-            for word in title.split(' '):
+            for word in title.split():
                 if word in word_list:
-                    found_list.append(word)
+                    if word in found_dict:
+                        found_dict[word] += 1
+                    else:
+                        found_dict[word] = 1
+
         if aft is not None:
-            count_words(subreddit, word_list, found_list, aft)
+            count_words(subreddit, word_list, found_dict, aft)
         else:
-            result = {}
-            for word in found_list:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            for key, value in sorted(result.items(), key=lambda item: item[1],
-                                     reverse=True):
+            sorted_results = sorted(found_dict.items(), key=lambda item: (-item[1], item[0]))
+            for key, value in sorted_results:
                 print('{}: {}'.format(key, value))
     else:
+        print("Invalid subreddit or unable to fetch data.")
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        count_words(sys.argv[1], sys.argv[2:])
+
         return
